e7dab43eb2e2d2de733a0cb48b014de32ee67482
Mairis <mairis.leisavnieks@gmail.com>
2015-07-13 21:49:40 +0300

    Start page, done.

--- Scribo/mainForm.Designer.cs
+++ Scribo/mainForm.Designer.cs
@@ -30,94 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
-            this.mainTstrip = new System.Windows.Forms.ToolStrip();
-            this.newTstripBtn = new System.Windows.Forms.ToolStripButton();
-            this.openTstripBtn = new System.Windows.Forms.ToolStripButton();
-            this.saveTstripBtn = new System.Windows.Forms.ToolStripButton();
-            this.mainTxtBox = new System.Windows.Forms.TextBox();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.toolStripBackBtn = new System.Windows.Forms.ToolStripButton();
-            this.mainTstrip.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // mainTstrip
-            // 
-            this.mainTstrip.AutoSize = false;
-            this.mainTstrip.BackColor = System.Drawing.Color.White;
-            this.mainTstrip.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.mainTstrip.CanOverflow = false;
-            this.mainTstrip.GripMargin = new System.Windows.Forms.Padding(0);
-            this.mainTstrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
-            this.mainTstrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripBackBtn});
-            this.mainTstrip.Location = new System.Drawing.Point(0, 0);
-            this.mainTstrip.Name = "mainTstrip";
-            this.mainTstrip.Padding = new System.Windows.Forms.Padding(0);
-            this.mainTstrip.Size = new System.Drawing.Size(484, 52);
-            this.mainTstrip.TabIndex = 0;
-            // 
-            // newTstripBtn
-            // 
-            this.newTstripBtn.AutoSize = false;
-            this.newTstripBtn.Image = ((System.Drawing.Image)(resources.GetObject("newTstripBtn.Image")));
-            this.newTstripBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.newTstripBtn.Margin = new System.Windows.Forms.Padding(1, 0, 1, 1);
-            this.newTstripBtn.Name = "newTstripBtn";
-            this.newTstripBtn.Size = new System.Drawing.Size(48, 48);
-            this.newTstripBtn.Text = "&New";
-            this.newTstripBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.newTstripBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.newTstripBtn.ToolTipText = "New file";
-            // 
-            // openTstripBtn
-            // 
-            this.openTstripBtn.AutoSize = false;
-            this.openTstripBtn.Image = ((System.Drawing.Image)(resources.GetObject("openTstripBtn.Image")));
-            this.openTstripBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.openTstripBtn.Margin = new System.Windows.Forms.Padding(1, 0, 1, 1);
-            this.openTstripBtn.Name = "openTstripBtn";
-            this.openTstripBtn.Size = new System.Drawing.Size(48, 48);
-            this.openTstripBtn.Text = "&Open";
-            this.openTstripBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.openTstripBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.openTstripBtn.ToolTipText = "Open a file";
-            // 
-            // saveTstripBtn
-            // 
-            this.saveTstripBtn.AutoSize = false;
-            this.saveTstripBtn.Image = ((System.Drawing.Image)(resources.GetObject("saveTstripBtn.Image")));
-            this.saveTstripBtn.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.saveTstripBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.saveTstripBtn.Margin = new System.Windows.Forms.Padding(1, 0, 1, 1);
-            this.saveTstripBtn.Name = "saveTstripBtn";
-            this.saveTstripBtn.Size = new System.Drawing.Size(48, 48);
-            this.saveTstripBtn.Text = "&Save";
-            this.saveTstripBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.saveTstripBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.saveTstripBtn.ToolTipText = "Save the current file";
-            // 
-            // mainTxtBox
-            // 
-            this.mainTxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.mainTxtBox.BackColor = System.Drawing.Color.White;
-            this.mainTxtBox.Location = new System.Drawing.Point(0, 52);
-            this.mainTxtBox.Multiline = true;
-            this.mainTxtBox.Name = "mainTxtBox";
-            this.mainTxtBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.mainTxtBox.Size = new System.Drawing.Size(484, 409);
-            this.mainTxtBox.TabIndex = 1;
-            // 
-            // toolStripBackBtn
-            // 
-            this.toolStripBackBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripBackBtn.Image = ((System.Drawing.Image)(resources.GetObject("toolStripBackBtn.Image")));
-            this.toolStripBackBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripBackBtn.Name = "toolStripBackBtn";
-            this.toolStripBackBtn.Size = new System.Drawing.Size(23, 49);
-            this.toolStripBackBtn.Text = "backBtn";
-            this.toolStripBackBtn.Click += new System.EventHandler(this.toolStripBackBtn_Click);
             // 
             // mainForm
             // 
@@ -125,30 +38,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(484, 461);
-            this.Controls.Add(this.mainTxtBox);
-            this.Controls.Add(this.mainTstrip);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MinimumSize = new System.Drawing.Size(500, 500);
             this.Name = "mainForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Scribo";
-            this.mainTstrip.ResumeLayout(false);
-            this.mainTstrip.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
         #endregion
-
-        private System.Windows.Forms.ToolStrip mainTstrip;
-        private System.Windows.Forms.TextBox mainTxtBox;
-        private System.Windows.Forms.ToolStripButton saveTstripBtn;
-        private System.Windows.Forms.ToolStripButton newTstripBtn;
-        private System.Windows.Forms.ToolStripButton openTstripBtn;
-        private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.ToolStripButton toolStripBackBtn;
     }
 }
 
--- Scribo/mainForm.cs
+++ Scribo/mainForm.cs
@@ -1,5 +1,6 @@
 ﻿using Scribo.Resources;
 using Scribo.Document;
+using Scribo.UI;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,10 +15,6 @@ namespace Scribo
 {
     public partial class mainForm : Form
     {
-        private List<Control> startPageCtl = new List<Control>();
-        private Image startPageNewImage;
-        private Image startPageOpenImage;
-        private Image startPageRecentImage;
 
         public mainForm()
         {
@@ -32,191 +29,7 @@ namespace Scribo
             //Loading the resourceManager and getting the needed resources
             resourceManager rm = new resourceManager();
             this.Icon = rm.mainFormIcon;
-            startPageNewImage = rm.startPageNewImage;
-            startPageOpenImage = rm.startPageOpenImage;
-            startPageRecentImage = rm.startPageRecentImage;
-            loadStartPage(true);
-        }
-
-        private void loadStartPage(bool firstTime)
-        {
-            //Hiding all the previous controls
-            this.Controls.Remove(mainTstrip);
-            this.Controls.Remove(mainTxtBox);
-
-            for (int i = 0; i < 3; i++)
-            {
-                if (firstTime)
-                {
-                    recentDocuments recentDocs = new recentDocuments(true);
-                    readDoc readDoc = new readDoc();
-
-                    //Setting up the panel  
-                    Panel startPagePnl = new Panel();
-                    startPagePnl.Width = this.Width / 3 - 50;
-                    startPagePnl.Height = startPagePnl.Width;
-                    startPagePnl.Anchor = (AnchorStyles.Top & AnchorStyles.Left);
-                    startPagePnl.Top = this.Height / 2 - startPagePnl.Height + 10;
-                    startPagePnl.Left = 50;
-                    startPagePnl.BackColor = Color.White;
-
-                    //Handling startPagePnl events
-                    startPagePnl.MouseEnter += (object sender, EventArgs e) =>
-                    { Panel p = (Panel)sender; p.BackColor = Color.LightGray; };
-                    startPagePnl.MouseLeave += (object sender, EventArgs e) =>
-                    { Panel p = (Panel)sender; p.BackColor = Color.White; };
-                    startPagePnl.Click += (object sender, EventArgs e) =>
-                    {
-                        Panel p = (Panel)sender;
-                        if (p.Tag == "newPnl")
-                        {
-                            loadTextEditor(true);
-                        }
-                        else if (p.Tag == "openPnl")
-                        {
-                            loadTextEditor(true);
-                            readDoc.showOfd(mainTxtBox);
-                        }
-                        else if (p.Tag == "recentPnl")
-                        {
-                            recentDocs.displayRecentsPanel(this);
-                        }
-                    };
-
-                    //Settings up the picture box
-                    PictureBox startPagePicBox = new PictureBox();
-                    startPagePicBox.Width = 80;
-                    startPagePicBox.Height = 80;
-                    startPagePicBox.Left = 18;
-                    startPagePicBox.Top = 6;
-                    startPagePicBox.BackColor = Color.Transparent;
-
-                    //Handling startPagePicBox events
-                    startPagePicBox.MouseEnter += (object sender, EventArgs e) =>
-                    { startPagePnl.BackColor = Color.LightGray; };
-                    startPagePicBox.MouseLeave += (object sender, EventArgs e) =>
-                    { startPagePnl.BackColor = Color.White; };
-                    startPagePicBox.Click += (object sender, EventArgs e) =>
-                    {
-                        PictureBox picBox = (PictureBox)sender;
-                        if (picBox.Tag == "newPicBox")
-                        {
-                            loadTextEditor(true);
-                        }
-                        else if (picBox.Tag == "openPicBox")
-                        {
-                            loadTextEditor(true);
-                            readDoc.showOfd(mainTxtBox);
-                        }
-                        else if (picBox.Tag == "recentPicBox")
-                        {
-                            recentDocs.displayRecentsPanel(this);
-                        }
-                    };
-
-                    //Setting up the label
-                    Label startPageLbl = new Label();
-                    startPageLbl.AutoSize = false;
-                    startPageLbl.BackColor = Color.Transparent;
-                    startPageLbl.Width = startPagePnl.Width;
-                    startPageLbl.Height = 22;
-                    startPageLbl.Font = new Font("Segoe UI", 10, FontStyle.Bold);
-                    startPageLbl.Top = startPageLbl.Height * 4;
-                    startPageLbl.TextAlign = ContentAlignment.MiddleCenter;
-
-                    //Handling startPageLbl events
-                    startPageLbl.MouseEnter += (object sender, EventArgs e) =>
-                    { startPagePnl.BackColor = Color.LightGray; };
-                    startPageLbl.MouseLeave += (object sender, EventArgs e) =>
-                    { startPagePnl.BackColor = Color.White; };
-                    startPageLbl.Click += (object sender, EventArgs e) =>
-                    {
-                        Label l = (Label)sender;
-                        if (l.Tag == "newLbl")
-                        {
-                            loadTextEditor(true);
-                        }
-                        else if (l.Tag == "openLbl")
-                        {
-                            loadTextEditor(true);
-                            readDoc.showOfd(mainTxtBox);
-                        }
-                        else if (l.Tag == "recentLbl")
-                        {
-                            recentDocs.displayRecentsPanel(this);
-                        }
-                    };
-
-                    //Adding controls to their parents
-                    this.Controls.Add(startPagePnl);
-                    startPagePnl.Controls.Add(startPagePicBox);
-                    startPagePnl.Controls.Add(startPageLbl);
-
-                    //A few individual properties
-                    switch (i)
-                    {
-                        case (0):
-                            startPagePicBox.Image = startPageNewImage;
-                            startPageLbl.Text = "New";
-                            startPagePnl.Tag = "newPnl";
-                            startPagePicBox.Tag = "newPicBox";
-                            startPageLbl.Tag = "newLbl";
-                            break;
-                        case (1):
-                            startPagePnl.Left = startPagePnl.Left + startPagePnl.Width + 20;
-                            startPagePicBox.Image = startPageOpenImage;
-                            startPageLbl.Text = "Open";
-                            startPagePnl.Tag = "openPnl";
-                            startPagePicBox.Tag = "openPicBox";
-                            startPageLbl.Tag = "openLbl";
-                            break;
-                        case (2):
-                            startPagePnl.Left = startPagePnl.Left * 2 + startPagePnl.Width * 2 - 10;
-                            startPagePicBox.Image = startPageRecentImage;
-                            startPageLbl.Text = "Open recent";
-                            startPagePnl.Tag = "recentPnl";
-                            startPagePicBox.Tag = "recentPicBox";
-                            startPageLbl.Tag = "recentLbl";
-                            break;
-                    }
-
-                    foreach (Control ctl in this.Controls)
-                    {
-                        if (ctl.Visible)
-                        {
-                            startPageCtl.Add(ctl);
-                        }
-                    }
-                }
-                else
-                {
-                    this.Controls.Remove(mainTstrip);
-                    this.Controls.Remove(mainTxtBox);
-                    foreach (Control ctl in startPageCtl)
-                    {
-                        this.Controls.Add(ctl);
-                    }
-                }
-            }
-        }
-
-        private void loadTextEditor(bool clearText)
-        {
-            if (clearText)
-            {
-                mainTxtBox.Text = "";
-            }
-            foreach (Control ctl in startPageCtl)
-            {
-                this.Controls.Remove(ctl);
-            }
-            this.Controls.Add(mainTstrip);
-            this.Controls.Add(mainTxtBox);
-        }
-
-        private void toolStripBackBtn_Click(object sender, EventArgs e)
-        {
-            loadStartPage(false);
+            startPage sP = new startPage(true, this);
         }
     }
 }
